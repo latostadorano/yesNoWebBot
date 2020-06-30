ed4501c36432ecdea3a44c81551d4125a785c9ba
latostadorano <bolsadepapel@hotmail.com>
2020-06-30 13:42:49 -0500

    Update yesNoBot.js

--- yesNoBot.js
+++ yesNoBot.js
@@ -1,45 +1,54 @@
-// Random yes no gif generator
-// We're working with Giphy's API.
-// User asks a question and the program will return a yesGif or a noGif and text.
+// Array de posibles respuestas: Yes or No; Tenemos que agregar más respuestas de cada opción
+//s Creamos una variable en la que abrá dos resultados: 0=Yes; 1=No
 
-var yesorno = ["Yes", "No"];                                                                                 //Array of multiple answears
-var yesText = ["For sure", "Definetly", "Fuck yeah!", "Affirmative", "Sounds good!", "A huevo!"];            //Array of multiple options for text
-var noText = ["Under no circumstances", "Not likely", "Thumbs down", "Nope", "Nel compa", "Fuck no"];        //Array of multiple options for text
+/* TAREAS: 
+  - Hacer que los gifs no se acumulen
+*/
+var img;
+var yesorno = ["Yes", "No"];                    // Arrays de respuestas
+var yesText = ["Yes","For sure", "Definetly", "Fuck yeah!", "Affirmative", "Sounds good!", "¡A huevo!"];
+var noText = ["Nay", "No way", "Negative", "Nope", "Nah", "Not a chance", "Noup"];
 
 var api = "http://api.giphy.com/v1/gifs/random?";
-var apiKey = "#####";
-var query = "&tag=";                           // tag = keyWord (in this case: Yes or No)
+var apiKey = "&api_key=oItqUmCXwyEWJrS34WHmC9cWuvtckyRQ";
+var query = "&tag=";                            // tag = palabra clave, g = rating
+var rating = "&g=1";
+
+var respuesta;
 
 function setup() {
+  clear ();
   var input = select('#question');
   input.changed(ask);
   var button = select('#submit');
-  //button.mousePressed(ask);                  If we activate this line we'll get a double answer and we don't want that
+  gif = select ('foto');
+  respuesta = select ('#h2');
+  
 }
 
 function ask() {
-  indexYesorno = floor(random(0, 2));           // Yes or no
+  indexYesorno = floor(random(0, 2));           // ésto hace el random de la respuesta
   var tag = yesorno[indexYesorno];
-
-  indexYes = floor(random(yesText.length));     // Random Yes text
+  indexYes = floor(random(yesText.length));    // ésto hace el random de texto que aparecerá
   var yesParagraph = yesText[indexYes];
-
-  indexNo = floor(random(noText.length));       //  Random No text
+  indexNo = floor(random(noText.length));      // ésto hace el random de texto que aparecerá
   var noParagraph = noText[indexNo];
 
-  var url = api + apiKey + query + tag;         //  Getting gif link from the Giphy API
+  var url = api + apiKey + query + tag + rating;
   loadJSON(url, gotData);
 
   if (tag == yesorno[0]) {
-    createP(yesParagraph);
-    print(tag);
-  }
-  if (tag == yesorno[1]) {
-    createP(noParagraph);
-    print(tag);
+    indexYes = floor(random(yesText.length)); // ésto hace el random de texto que aparecerá
+    var yesParagraph = yesText[indexYes];
+    respuesta.html(yesParagraph);
+  } else if (tag == yesorno[1]) {
+    indexNo = floor(random(noText.length)); // ésto hace el random de texto que aparecerá
+    var noParagraph = noText[indexNo];
+    respuesta.html(noParagraph);
   }
 }
 
-function gotData(giphy) {                         //  Create gif function
-  createImg((giphy.data.images.original.url));
+function gotData(giphy) {
+  document.getElementById("foto").src = (giphy.data.images.original.url);
+
 }
